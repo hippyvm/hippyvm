5ad41fc95211e06a2f5abc78057dfe04037ccea4
Ashwini Oruganti <ashwini.oruganti@gmail.com>
2014-05-08 01:43:01 -0700

    Test addEmptyDir()
    --HG--
    branch : phar

--- testing/test_phar.py
+++ testing/test_phar.py
@@ -1,4 +1,6 @@
+import tempfile
 from testing.test_interpreter import BaseTestInterpreter
+
 
 class TestPhar(BaseTestInterpreter):
 
@@ -20,3 +22,19 @@ class TestPhar(BaseTestInterpreter):
         assert len(output) == 2
         assert self.space.str_w(output[0]) == 'file1.txt'
         assert self.space.str_w(output[1]) == 'Information'
+
+    def test_add_empty_dir(self):
+        tempdir = tempfile.mkdtemp()
+        output = self.run('''
+            if (Phar::canWrite()) {
+            $p = new Phar('newphar.tar.phar', 0, 'newphar.tar.phar');
+            $p->startBuffering();
+            $p['file1.txt'] = 'Information';
+            $p->stopBuffering();
+            }
+
+            $p2 = new Phar('newphar.tar.phar', 0);
+            $p2->addEmptyDir('%s');
+            echo $p2['%s']->isDir();        # TODO: Catch exception on failure
+            ''' % (tempdir, tempdir))
+        assert output[0] == self.space.w_True
