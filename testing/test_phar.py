ba245f6ab56c6f667ff00e8d04aceddd2c985b93
Ashwini Oruganti <ashwini.oruganti@gmail.com>
2014-05-08 02:00:07 -0700

    Test addFile()
    --HG--
    branch : phar

--- testing/test_phar.py
+++ testing/test_phar.py
@@ -1,3 +1,4 @@
+import py
 import tempfile
 from testing.test_interpreter import BaseTestInterpreter
 
@@ -7,13 +8,13 @@ class TestPhar(BaseTestInterpreter):
     def test_create_phar(self):
         output = self.run('''
             if (Phar::canWrite()) {
-            $p = new Phar('newphar.tar.phar', 0, 'newphar.tar.phar');
+            $p = new Phar('newphar0.tar.phar', 0, 'newphar0.tar.phar');
             $p->startBuffering();
             $p['file1.txt'] = 'Information';
             $p->stopBuffering();
             }
 
-            $p2 = new Phar('newphar.tar.phar', 0);
+            $p2 = new Phar('newphar0.tar.phar', 0);
             foreach (new RecursiveIteratorIterator($p2) as $file) {
                 echo $file->getFileName();
                 echo file_get_contents($file->getPathName());
@@ -27,14 +28,24 @@ class TestPhar(BaseTestInterpreter):
         tempdir = tempfile.mkdtemp()
         output = self.run('''
             if (Phar::canWrite()) {
-            $p = new Phar('newphar.tar.phar', 0, 'newphar.tar.phar');
+            $p = new Phar('newphar1.tar.phar', 0, 'newphar1.tar.phar');
             $p->startBuffering();
             $p['file1.txt'] = 'Information';
             $p->stopBuffering();
             }
 
-            $p2 = new Phar('newphar.tar.phar', 0);
+            $p2 = new Phar('newphar1.tar.phar', 0);
             $p2->addEmptyDir('%s');
-            echo $p2['%s']->isDir();        # TODO: Catch exception on failure
+            echo $p2['%s']->isDir();        // TODO: Catch exception on failure
             ''' % (tempdir, tempdir))
         assert output[0] == self.space.w_True
+
+    def test_add_file(self):
+        f = py.path.local(tempfile.mkstemp()[1])
+        f.write('Foo')
+        output = self.run('''
+            $p = new Phar('newphar2.tar.phar', 0, 'newphar2.tar.phar');
+            $p->addFile('%s');
+            echo $p['%s']->getContent();
+            ''' % (f, f))
+        assert self.space.str_w(output[0]) == 'Foo'
