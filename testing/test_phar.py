96134c4e3f0215609eb07ba4fcc93339326cc6c4
Ashwini Oruganti <ashwini.oruganti@gmail.com>
2014-05-12 07:14:16 -0700

    Clean up after your test
    --HG--
    branch : phar

--- testing/test_phar.py
+++ testing/test_phar.py
@@ -31,15 +31,18 @@ class TestPhar(BaseTestInterpreter):
         tempdir = tempfile.mkdtemp()
         output = self.run('''
         if (Phar::canWrite()) {
-        $p = new Phar('/tmp/newphar1.tar.phar', 0, 'newphar1.tar.phar');
+        $p = new Phar('/tmp/newphar.tar.phar', 0, 'newphar.tar.phar');
         $p->startBuffering();
         $p['file1.txt'] = 'Information';
         $p->stopBuffering();
         }
 
-        $p2 = new Phar('newphar1.tar.phar', 0);
+        $p2 = new Phar('newphar.tar.phar', 0);
         $p2->addEmptyDir('%s');
         echo $p2['%s']->isDir();        // TODO: Catch exception on failure
+        unset($p);
+        unset($p2);
+        Phar::unlinkArchive('/tmp/newphar.tar.phar');
         ''' % (tempdir, tempdir))
         assert output[0] == self.space.w_True
 
@@ -47,18 +50,22 @@ class TestPhar(BaseTestInterpreter):
         f = py.path.local(tempfile.mkstemp()[1])
         f.write('Foo')
         output = self.run('''
-        $p = new Phar('/tmp/newphar2.tar.phar', 0, 'newphar2.tar.phar');
+        $p = new Phar('/tmp/newphar.tar.phar', 0, 'newphar.tar.phar');
         $p->addFile('%s');
         echo $p['%s']->getContent();
+        unset($p);
+        Phar::unlinkArchive('/tmp/newphar.tar.phar');
         ''' % (f, f))
         assert self.space.str_w(output[0]) == 'Foo'
 
     def test_add_from_string(self):
         output = self.run('''
-        $p = new Phar('/tmp/newphar3.tar.phar', 0, 'newphar3.tar.phar');
+        $p = new Phar('/tmp/newphar.tar.phar', 0, 'newphar.tar.phar');
         $p->addFromString('test/path/foo.txt', 'Bar');
         echo $p['test/path/foo.txt']->getContent();
-        ''')
+        unset($p);
+        Phar::unlinkArchive('/tmp/newphar.tar.phar');
+       ''')
         assert self.space.str_w(output[0]) == 'Bar'
 
     def test_api_version(self):
@@ -67,7 +74,7 @@ class TestPhar(BaseTestInterpreter):
         ''')
         assert self.space.str_w(output[0]) == '1.1.1'       # TODO: Will this be true always?
 
-    def test_build_from_directory(self):
+    def test_build_from_directory(self):            # XXX: Doesn't work on first run
         tempdir = py.path.local(tempfile.mkdtemp())
         f = tempdir.join('foo.txt')
         f.write('test file 1')
@@ -80,7 +87,10 @@ class TestPhar(BaseTestInterpreter):
             echo $file->getFileName();
             echo file_get_contents($file->getPathName());
             }
-       ''' % f)
+//        unset($p);
+//        Phar::unlinkArchive('/tmp/newphar.tar.phar');
+        ''' % f)
+
         assert len(output) == 4
         assert self.space.newstr('foo.txt') in output
         assert self.space.newstr('test file 1') in output
@@ -89,17 +99,19 @@ class TestPhar(BaseTestInterpreter):
 
     def test_offset_exists(self):
         output = self.run('''
-        $p = new Phar('/tmp/newphar5.tar.phar', 0, 'newphar5.tar.phar');
+        $p = new Phar('/tmp/newphar.tar.phar', 0, 'newphar.tar.phar');
         $p['foo.txt'] = 'Foo.';
         echo isset($p['foo.txt']);
         echo isset($p['bar.txt']);
+        unset($p);
+        Phar::unlinkArchive('/tmp/newphar.tar.phar');
         ''')
         assert output[0] == self.space.w_True
         assert output[1] == self.space.w_False
 
     def test_offset_get(self):
         output = self.run('''
-        $p = new Phar('/tmp/newphar6.tar.phar', 0, 'newphar6.tar.phar');
+        $p = new Phar('/tmp/newphar.tar.phar', 0, 'newphar.tar.phar');
         $p['foo.txt'] = "File exists";
         try {
             $res = $p['foo.txt'];
@@ -109,6 +121,8 @@ class TestPhar(BaseTestInterpreter):
         } catch (BadMethodCallException $e) {
             echo $e->getMessage();
         }
+        unset($p);
+        Phar::unlinkArchive('/tmp/newphar.tar.phar');
         ''')
         assert self.space.str_w(output[0]) == 'PharFileInfo'
         assert self.space.str_w(output[1]) == 'File exists'
@@ -116,7 +130,7 @@ class TestPhar(BaseTestInterpreter):
 
     def test_offset_unset(self):
         output = self.run('''
-        $p = new Phar('/tmp/newphar7.tar.phar', 0, 'newphar7.tar.phar');
+        $p = new Phar('/tmp/newphar.tar.phar', 0, 'newphar.tar.phar');
         $p['foo.txt'] = "File exists";
         try {
             echo $p['foo.txt']->getContent();
@@ -125,29 +139,35 @@ class TestPhar(BaseTestInterpreter):
         } catch (BadMethodCallException $e) {
             echo $e->getMessage();
         }
+        unset($p);
+        Phar::unlinkArchive('/tmp/newphar.tar.phar');
         ''')
         assert self.space.str_w(output[0]) == 'File exists'
         assert self.space.str_w(output[1]) == 'Entry foo.txt does not exist'
 
     def test_is_buffering(self):
         output = self.run('''
-        $p1 = new Phar('/tmp/newphar8.tar.phar', 0, 'newphar8.tar.phar');
+        $p1 = new Phar('/tmp/newphar1.tar.phar', 0, 'newphar1.tar.phar');
         $p1['foo.txt'] = "Foo";
-        $p2 = new Phar('/tmp/newphar9.tar.phar', 0, 'newphar9.tar.phar');
+        $p2 = new Phar('/tmp/newphar2.tar.phar', 0, 'newphar2.tar.phar');
         $p2['bar.txt'] = "Bar";
         $p1->startBuffering();
         echo $p1->isBuffering();
         echo $p2->isBuffering();
         $p1->stopBuffering();
         echo $p1->isBuffering();
+        unset($p1);
+        unset($p2);
+        Phar::unlinkArchive('/tmp/newphar1.tar.phar');
+        Phar::unlinkArchive('/tmp/newphar2.tar.phar');
         ''')
         assert output[0] == self.space.w_True
         assert output[1] == self.space.w_False
         assert output[2] == self.space.w_False
 
-    def test_compress_files_gz(self):
+    def test_compress_files_gz(self):       # XXX: Doesn't pass on first runs
         output = self.run('''
-        $p = new Phar('/tmp/newphar10.phar', 0, 'newphar10.phar');
+        $p = new Phar('/tmp/newphar000.phar', 0, 'newphar000.phar');
         $p['myfile1.txt'] = 'Foo.';
         $p['myfile2.txt'] = 'Bar.';
         foreach ($p as $file) {
@@ -161,6 +181,8 @@ class TestPhar(BaseTestInterpreter):
             echo $file->isCompressed(Phar::GZ);
             echo $file->isCompressed(Phar::BZ2);
         }
+//        unset($p);
+//        Phar::unlinkArchive('/tmp/newphar000.phar');
         ''')
         assert len(output) == 12
         for i in range(6):
@@ -172,9 +194,10 @@ class TestPhar(BaseTestInterpreter):
             assert output[i+2] == self.space.w_False
             i = i + 3
 
-    def test_compress_files_bz2(self):
+    def test_compress_files_bz2(self):      # XXX: Doesn't pass on first runs
+
         output = self.run('''
-        $p = new Phar('/tmp/newphar11.phar', 0, 'newphar11.phar');
+        $p = new Phar('/tmp/newphar001.phar', 0, 'newphar001.phar');
         $p['myfile1.txt'] = 'Foo.';
         $p['myfile2.txt'] = 'Bar.';
         foreach ($p as $file) {
@@ -188,6 +211,8 @@ class TestPhar(BaseTestInterpreter):
             echo $file->isCompressed(Phar::GZ);
             echo $file->isCompressed(Phar::BZ2);
         }
+//        unset($p);
+//        Phar::unlinkArchive('/tmp/newphar001.phar');
         ''')
         assert len(output) == 12
         for i in range(6):
@@ -201,41 +226,47 @@ class TestPhar(BaseTestInterpreter):
 
     def test_compress_gz(self):
         output = self.run('''
-        $p = new Phar('/tmp/newphar12.phar', 0, 'newphar12.phar');
+        $p = new Phar('/tmp/newphar.phar', 0, 'newphar.phar');
         $p['myfile1.txt'] = 'Foo';
         $p['myfile2.txt'] = 'Bar';
         $p1 = $p->compress(Phar::GZ);
         echo get_class($p1);
         echo $p1->isCompressed();
+        unset($p);
         unset($p1);
-        Phar::unlinkArchive('/tmp/newphar12.phar.gz');
+        Phar::unlinkArchive('/tmp/newphar.phar');
+        Phar::unlinkArchive('/tmp/newphar.phar.gz');
         ''')
         assert self.space.str_w(output[0]) == 'Phar'
         assert self.space.int_w(output[1]) == 4096
 
     def test_compress_bz2(self):
         output = self.run('''
-        $p = new Phar('/tmp/newphar13.phar', 0, 'newphar13.phar');
+        $p = new Phar('/tmp/newphar.phar', 0, 'newphar.phar');
         $p['myfile1.txt'] = 'Foo';
         $p['myfile2.txt'] = 'Bar';
         $p1 = $p->compress(Phar::BZ2);
         echo get_class($p1);
         echo $p1->isCompressed();
+        unset($p);
         unset($p1);
-        Phar::unlinkArchive('/tmp/newphar13.phar.bz2');
+        Phar::unlinkArchive('/tmp/newphar.phar');
+        Phar::unlinkArchive('/tmp/newphar.phar.bz2');
         ''')
         assert self.space.str_w(output[0]) == 'Phar'
         assert self.space.int_w(output[1]) == 8192
 
     def test_compress_none(self):
         output = self.run('''
-        $p = new Phar('/tmp/newphar14.phar', 0, 'newphar14.phar');
+        $p = new Phar('/tmp/newphar.phar', 0, 'newphar.phar');
         $p['myfile1.txt'] = 'Foo';
         $p['myfile2.txt'] = 'Bar';
         try{
-            $p1 = $p->compress(Phar::NONE); // copies to /path/to/my.phar.bzz
+            $p1 = $p->compress(Phar::NONE);
         } catch (BadMethodCallException $e) {
             echo $e->getMessage();
         }
+        unset($p);
+        Phar::unlinkArchive('/tmp/newphar.phar');
         ''')
-        assert self.space.str_w(output[0]) == 'Unable to add newly converted phar "/tmp/newphar14.phar" to the list of phars, a phar with that name already exists'
+        assert self.space.str_w(output[0]) == 'Unable to add newly converted phar "/tmp/newphar.phar" to the list of phars, a phar with that name already exists'
