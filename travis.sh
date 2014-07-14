a0c9c6b9b7f6f2e299fc9f57bcef4834b1a1feef
xando <sebastian.pawlus@gmail.com>
2014-07-14 16:08:51 +0200

    travis execution moved to separate file

---
+++ travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:pypy
+
+
+declare -a STEPS=(
+    'py.test testing/'
+    'python pypy/rpython/bin/rpython targethippy.py'
+    'py.test test_phpt'
+);
+
+
+for var in "${STEPS[@]}"
+do
+    echo -e "\n\e[32m#### $var ####\e[0m\n"
+    $var
+    if [ $? != 0 ]
+    then
+	exit $?
+    fi
+
+done
