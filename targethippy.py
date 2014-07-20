a137321c72943d9202eb63bef450739fb100985d
Edd Barrett <vext01@gmail.com>
2014-07-20 16:07:19 +0100

    When translating, Check that the extensions the user asked for are present.
    Before --withext-* (or --allexts) would silently be ignored if external
    extension source code was missing.

--- targethippy.py
+++ targethippy.py
@@ -12,8 +12,17 @@ def target(driver, args):
     parser = to_optparse(config, parserkwargs={'usage': SUPPRESS_USAGE })
     parser.parse_args(args)
 
+
+    from hippy.hippyoption import (
+        OPTIONAL_EXTS, is_optional_extension_enabled, HippyOptionError)
+
     from hippy.hippyoption import set_options
     set_options(config)
+
+    for ext in OPTIONAL_EXTS:
+        asked = getattr(config.optexts, ext)
+        if asked and not is_optional_extension_enabled(ext):
+            raise HippyOptionError("%s extension sources are missing!" % ext)
 
     from hippy.main import entry_point
     return entry_point, None
