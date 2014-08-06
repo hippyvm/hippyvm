e6938eac88f4317d643190f014039cdba9b0a485
soda <rafal.galczynski@gmail.com>
2014-08-06 14:24:41 +0200

    register xmlparserresource

--- hippy/objspace.py
+++ hippy/objspace.py
@@ -100,12 +100,13 @@ class ObjSpace(object):
     (tp_int, tp_float, tp_str, tp_array, tp_null, tp_bool,
      tp_object, tp_file_res, tp_dir_res, tp_stream_context,
      tp_mysql_link, tp_mysql_result,
-     tp_constant, tp_delayed_class_const) = range(14)
+     tp_constant, tp_delayed_class_const, tp_xmlparser_res) = range(15)
 
     # in the same order as the types above
     TYPENAMES = ["integer", "double", "string", "array", "NULL", "boolean",
                  "object", "resource", "resource", "resource",
-                 "resource", "resource", "constant", "delayed constant"]
+                 "resource", "resource", "constant", "delayed constant",
+                 "resource"]
 
     w_True = w_True
     w_False = w_False
@@ -947,6 +948,10 @@ if is_optional_extension_enabled("mysql"):
     from ext_module.mysql.result_resource import W_MysqlResultResource
     W_MysqlLinkResource.tp = ObjSpace.tp_mysql_link
     W_MysqlResultResource.tp = ObjSpace.tp_mysql_result
+
+if is_optional_extension_enabled("xml"):
+    from ext_module.xml.xmlparser import XMLParserResource
+    XMLParserResource.tp = ObjSpace.tp_xmlparser_res
 
 W_Constant.tp = ObjSpace.tp_constant
 DelayedClassConstant.tp = ObjSpace.tp_delayed_class_const
