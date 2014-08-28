8fa45d345611894885a5d1b91fe3e4db8564e5ac
Ronan Lamy <ronan.lamy@gmail.com>
2014-08-28 06:10:10 +0100

    implement all IteratorIterator methods in the simple case

--- hippy/module/spl/iterator.py
+++ hippy/module/spl/iterator.py
@@ -1,33 +1,52 @@
 from hippy.klass import def_class
-from hippy.builtin import ExitFunctionWithError, wrap_method
+from hippy.builtin import wrap_method, ThisUnwrapper
+from hippy.objects.instanceobject import W_InstanceObject
 from hippy.module.spl.interface import k_OuterIterator
 
 
-@wrap_method(['interp', 'this'], name='IteratorIterator::current')
+class W_IteratorIterator(W_InstanceObject):
+    inner = None
+
+
+@wrap_method(['interp', ThisUnwrapper(W_IteratorIterator), 'object'],
+             name='IteratorIterator::__construct')
+def ii_construct(interp, this, iterator):
+    this.inner = iterator
+
+
+@wrap_method(['interp', ThisUnwrapper(W_IteratorIterator)],
+             name='IteratorIterator::current')
 def ii_current(interp, this):
-    raise ExitFunctionWithError('Not yet implemented in hippy')
+    return interp.getmeth(this.inner, 'current').call_args(interp, [])
 
-@wrap_method(['interp', 'this'], name='IteratorIterator::getInnerIterator')
+@wrap_method(['interp', ThisUnwrapper(W_IteratorIterator)],
+             name='IteratorIterator::getInnerIterator')
 def ii_getInnerIterator(interp, this):
-    raise ExitFunctionWithError('Not yet implemented in hippy')
+    return this.inner
 
-@wrap_method(['interp', 'this'], name='IteratorIterator::key')
+@wrap_method(['interp', ThisUnwrapper(W_IteratorIterator)],
+             name='IteratorIterator::key')
 def ii_key(interp, this):
-    raise ExitFunctionWithError('Not yet implemented in hippy')
+    return interp.getmeth(this.inner, 'key').call_args(interp, [])
 
-@wrap_method(['interp', 'this'], name='IteratorIterator::next')
+@wrap_method(['interp', ThisUnwrapper(W_IteratorIterator)],
+             name='IteratorIterator::next')
 def ii_next(interp, this):
-    raise ExitFunctionWithError('Not yet implemented in hippy')
+    return interp.getmeth(this.inner, 'next').call_args(interp, [])
 
-@wrap_method(['interp', 'this'], name='IteratorIterator::rewind')
+@wrap_method(['interp', ThisUnwrapper(W_IteratorIterator)],
+             name='IteratorIterator::rewind')
 def ii_rewind(interp, this):
-    raise ExitFunctionWithError('Not yet implemented in hippy')
+    return interp.getmeth(this.inner, 'rewind').call_args(interp, [])
 
-@wrap_method(['interp', 'this'], name='IteratorIterator::valid')
+@wrap_method(['interp', ThisUnwrapper(W_IteratorIterator)],
+             name='IteratorIterator::valid')
 def ii_valid(interp, this):
-    raise ExitFunctionWithError('Not yet implemented in hippy')
+    return interp.getmeth(this.inner, 'valid').call_args(interp, [])
 
 k_IteratorIterator = def_class(
     'IteratorIterator',
-    [ii_current, ii_getInnerIterator, ii_key, ii_next, ii_rewind, ii_valid],
-    implements=[k_OuterIterator])
+    [ii_construct, ii_current, ii_getInnerIterator, ii_key, ii_next,
+     ii_rewind, ii_valid],
+    implements=[k_OuterIterator],
+    instance_class=W_IteratorIterator)
