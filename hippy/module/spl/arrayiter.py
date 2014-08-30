c892dd9df2d18c7e82770bd153491183dd64d0ed
Ronan Lamy <ronan.lamy@gmail.com>
2014-08-30 04:30:58 +0100

    Use def_method() for ArrayIterator

--- hippy/module/spl/arrayiter.py
+++ hippy/module/spl/arrayiter.py
@@ -1,57 +1,50 @@
 from hippy.builtin_klass import k_Iterator
-from hippy.builtin import ThisUnwrapper, Optional, wrap_method
+from hippy.builtin import Optional
 from hippy.klass import def_class
 from hippy.objects.base import W_Root
 from hippy.objects.instanceobject import W_InstanceObject
 from hippy import consts
 
 
-class W_ApplevelArrayIterator(W_InstanceObject):
-    pass
-
-
-@wrap_method(['interp', ThisUnwrapper(W_ApplevelArrayIterator),
-              Optional(W_Root)],
-             name='ArrayIterator::__construct')
-def ArrayIterator_construct(interp, this, w_arr=None):
-    if w_arr is None:
-        w_arr = interp.space.new_array_from_list([])
-    this.setattr(interp, "storage", w_arr, k_ArrayIterator)
-
-
-@wrap_method([], name='ArrayIterator::current')
-def ArrayIterator_current():
-    pass
-
-
-@wrap_method([], name='ArrayIterator::next')
-def ArrayIterator_next():
-    pass
-
-
-@wrap_method([], name='ArrayIterator::key')
-def ArrayIterator_key():
-    pass
-
-
-@wrap_method([], name='ArrayIterator::rewind')
-def ArrayIterator_rewind():
-    pass
-
-
-@wrap_method([], name='ArrayIterator::valid')
-def ArrayIterator_valid():
+class W_ArrayIterator(W_InstanceObject):
     pass
 
 
 k_ArrayIterator = def_class(
     'ArrayIterator',
-    [ArrayIterator_construct,
-     ArrayIterator_current,
-     ArrayIterator_next,
-     ArrayIterator_key,
-     ArrayIterator_rewind,
-     ArrayIterator_valid],
+    ['__construct', 'current', 'next', 'key', 'rewind', 'valid'],
     [('storage', consts.ACC_PRIVATE)],
-    instance_class=W_ApplevelArrayIterator,
+    instance_class=W_ArrayIterator,
     implements=[k_Iterator])
+
+
+@k_ArrayIterator.def_method(['interp', 'this', Optional(W_Root)])
+def __construct(interp, this, w_arr=None):
+    if w_arr is None:
+        w_arr = interp.space.new_array_from_list([])
+    this.setattr(interp, "storage", w_arr, k_ArrayIterator)
+
+
+@k_ArrayIterator.def_method([])
+def current():
+    pass
+
+
+@k_ArrayIterator.def_method([])
+def next():
+    pass
+
+
+@k_ArrayIterator.def_method([])
+def key():
+    pass
+
+
+@k_ArrayIterator.def_method([])
+def rewind():
+    pass
+
+
+@k_ArrayIterator.def_method([])
+def valid():
+    pass
