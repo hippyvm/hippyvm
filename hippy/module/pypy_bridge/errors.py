b818a9c2f61156f1a19df38297ed7b17d1283dc6
Edd Barrett <vext01@gmail.com>
2014-10-02 10:44:24 +0100

    Remove dead file: errors.py.

--- hippy/module/pypy_bridge/errors.py
+++
@@ -1,7 +0,0 @@
-from pypy.interpreter.error import OperationError
-
-def raise_python_bridge_error(interp, msg):
-    """ Raises BridgeError in Python """
-    py_space = interp.py_space
-    w_py_bridge_error = py_space.builtin.get("BridgeError")
-    raise OperationError(w_py_bridge_error, py_space.wrap(msg))
