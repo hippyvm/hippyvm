7fc9c5823fff9edcfc4c2e68889f8211ee092048
Edd Barrett <vext01@gmail.com>
2014-10-28 16:15:44 +0000

    Add another test, rename a var.

--- testing/test_pypy_bridge_arg_passing.py
+++ testing/test_pypy_bridge_arg_passing.py
@@ -374,8 +374,9 @@ class TestPyPyBridgeArgPassing(BaseTestInterpreter):
         output = self.run('''
         function takes_ref(&$x) {
             $src = "def mutate_ref(y): y = 666";
-            $mutate_ary_ref = embed_py_func($src);
-            $mutate_ary_ref($x);
+            $mutate_ref = embed_py_func($src);
+            $mutate_ref($x);
+            echo $x;
         }
 
         $a = 1;
@@ -383,3 +384,20 @@ class TestPyPyBridgeArgPassing(BaseTestInterpreter):
         echo $a;
         ''')
         assert php_space.int_w(output[0]) == 666
+        assert php_space.int_w(output[1]) == 666
+
+    def test_php2py_existing_ref_respected2(self, php_space):
+        output = self.run('''
+        function takes_ref(&$x) {
+            $src = "def mutate_ref(y): y = y + 1";
+            $mutate_ref = embed_py_func($src);
+            $mutate_ref($x);
+            echo $x;
+        }
+
+        $a = 1;
+        takes_ref($a);
+        echo $a;
+        ''')
+        assert php_space.int_w(output[0]) == 2
+        assert php_space.int_w(output[1]) == 2
