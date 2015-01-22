f3edf3d36a38b0d3ac33a711fcfb596052e1909f
Edd Barrett <vext01@gmail.com>
2015-01-22 14:11:16 +0000

    Add more tests involving objects.

--- testing/test_comparisons.py
+++ testing/test_comparisons.py
@@ -43,3 +43,212 @@ class TestComparisons(BaseTestInterpreter):
         echo $a == $b;
         """)
         assert not php_space.is_true(output[0])
+
+    def test_object_eq_object(self, php_space):
+        output = self.run("""
+        class X {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        $a = new X();
+        $b = new X();
+
+        echo $a == $b;
+        """)
+        assert php_space.is_true(output[0])
+
+    def test_object_eq_object_strict(self, php_space):
+        output = self.run("""
+        class X {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        $a = new X();
+        $b = new X();
+
+        echo $a === $b;
+        """)
+        assert php_space.is_true(output[0])
+
+    def test_object_not_eq_object_strict(self, php_space):
+        output = self.run("""
+        class X {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        class Y {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        $a = new X();
+        $b = new Y();
+
+        echo $a === $b;
+        """)
+        assert not php_space.is_true(output[0])
+
+    def test_object_diff_class_eq_object(self, php_space):
+        output = self.run("""
+        class X {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        class Y {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        $a = new X();
+        $b = new Y();
+
+        echo $a == $b;
+        """)
+        assert not php_space.is_true(output[0])
+
+    def test_object_diff_class_not_eq_object(self, php_space):
+        output = self.run("""
+        class X {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        class Y {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        $a = new X();
+        $a->f1 = 666;
+        $b = new Y();
+
+        echo $a == $b;
+        """)
+        assert not php_space.is_true(output[0])
+
+    def test_object_not_eq_object(self, php_space):
+        output = self.run("""
+        class X {
+            public $f1 = 2;
+            public $f2 = 3;
+        }
+
+        $a = new X();
+        $a->f1 = 666;
+        $b = new X();
+
+        echo $a == $b;
+        """)
+        assert not php_space.is_true(output[0])
+
+
+    def test_nested_object_eq_object(self, php_space):
+        output = self.run("""
+        class Node {
+            function __construct($l, $r) {
+                $this->l = $l;
+                $this->r = $r;
+            }
+        }
+
+        $tree1 = new Node(
+            new Node(
+                new Node(1, 2),
+                new Node(3, 4)
+            ),
+            new Node(
+                new Node(5, new Node(6, 7)),
+                8
+            )
+        );
+
+        $tree2 = new Node(
+            new Node(
+                new Node(1, 2),
+                new Node(3, 4)
+            ),
+            new Node(
+                new Node(5, new Node(6, 7)),
+                8
+            )
+        );
+
+        echo $tree1 == $tree2;
+        """)
+        assert php_space.is_true(output[0])
+
+    def test_nested_object_eq_object_strict(self, php_space):
+        output = self.run("""
+        class Node {
+            function __construct($l, $r) {
+                $this->l = $l;
+                $this->r = $r;
+            }
+        }
+
+        $tree1 = new Node(
+            new Node(
+                new Node(1, 2),
+                new Node(3, 4)
+            ),
+            new Node(
+                new Node(5, new Node(6, 7)),
+                8
+            )
+        );
+
+        $tree2 = new Node(
+            new Node(
+                new Node(1, 2),
+                new Node(3, 4)
+            ),
+            new Node(
+                new Node(5, new Node(6, 7)),
+                8
+            )
+        );
+
+        echo $tree1 === $tree2;
+        """)
+        assert php_space.is_true(output[0])
+
+    def test_nested_object_not_eq_object_strict(self, php_space):
+        output = self.run("""
+        class Node {
+            function __construct($l, $r) {
+                $this->l = $l;
+                $this->r = $r;
+            }
+        }
+
+        class Node1 extends Node {};
+
+        $tree1 = new Node(
+            new Node(
+                new Node(1, 2),
+                new Node(3, 4)
+            ),
+            new Node(
+                new Node(5, new Node(6, 7)),
+                8
+            )
+        );
+
+        $tree2 = new Node(
+            new Node(
+                new Node(1, 2),
+                new Node(3, 4)
+            ),
+            new Node(
+                new Node1(5, new Node(6, 7)), // differs
+                8
+            )
+        );
+
+        echo $tree1 === $tree2;
+        """)
+        assert not php_space.is_true(output[0])
