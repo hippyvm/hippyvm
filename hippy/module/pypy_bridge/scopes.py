3b4b1cf5480f92723b14932d8373368ef316fff1
Edd Barrett <vext01@gmail.com>
2015-02-19 17:46:07 +0000

    Eliminate quadratic behavior in PHP->Py calls.

--- hippy/module/pypy_bridge/scopes.py
+++ hippy/module/pypy_bridge/scopes.py
@@ -29,8 +29,6 @@ class PHP_Scope(WPy_Root):
         self.name_map = {}
         self.name_map_version = NameMapVersion()
 
-
-    @jit.elidable_promote()
     def _lookup_name_map(self, n, version):
         try:
             return self.name_map[n]
